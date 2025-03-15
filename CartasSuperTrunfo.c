d0ada20336e0352e6b1068b99f14e1e5e337a58b
kemilynalexy <yeonsangcute@gmail.com>
2025-03-15 23:52:29 +0000

    criei as informações da CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,42 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
+char estado[20];
+char codigo[50];
+char cidade[50];
+int populacao;
+float area;
+float pib;
+int turistco;
+
 
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("digite o estado: ");
+    scanf("%s", estado);
+    printf("o estado é: %s\n", estado);
+
+    printf("digite o código: ");
+    scanf("%s", codigo);
+    printf("o codigo é: %s\n", codigo);
+
+    printf("digite a cidade: ");
+    scanf("%s", cidade);
+    printf("a cidade é: %s\n", cidade);
+
+    printf("digite a população: ");
+    scanf("%d", &populacao);
+    printf("a população é: %d\n", populacao);
+
+    printf("digite a área: ");
+    scanf("%f", &area);
+    printf("a área é: %f\n", area);
+
+    printf("digite o PIB: ");
+    scanf("%f", &pib);
+    printf("o pib é: %f\n", pib);
+
+    printf("digite a quantidade de pontos turísticos: ");
+    scanf("%d", &turistco);
+    printf("a quantidade de pontos turísticos é: %d\n", turistco);
 
     return 0;
 }
